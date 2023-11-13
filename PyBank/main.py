a70da0b827196023db01c9f9bd137d3bee7628e7
Sanja Kostic <147453284+sanjakostic@users.noreply.github.com>
2023-11-12 21:05:26 -0800

    Updates to main.py files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,46 +1,69 @@
 import os
 import csv
 
-csvfile = '.\\Resources\\budget_data.csv'
+# create file path to read csv file
+csvfile = os.path.join(".","Resources", "budget_data.csv")
 
 full_data = list()
 
 with open(csvfile) as hshs:
     reader = csv.reader(hshs)
+
+    # for each row in the input file, append it to full_data (list)
     for row in reader:
         full_data.append(row)
 
 
-full_data_no_headers = full_data[1:]
+# remove header from rows, but store in a different variable
+full_data_no_headers = full_data[1:]  
 
+# number of months is equal to length of this data 
 no_months = len(full_data_no_headers)
 
+# initialize net_total
 net_total  = 0
 
+# for each row, add the value to net_total
 for i in full_data_no_headers:
     net_total = net_total + int(i[1])
 
-net_total
-
+# get a list of just the values from the full_data
 profs = [i[1] for i in full_data_no_headers]
+
+# initialize list for the changes (deltas) from month to month
 delts = list()
 ind_delts = dict()
 
+# for each value in the profits,
+# iterate through the months, get the difference between
+# the current month and the next month, and append it
+# to delts (list), and add it to dictionary ind_delts
+# with key i (the number/index of the month in profs,
+# which would follow the index in full_data_no_headers)
 for i in range(len(profs)-1):
     diff = int(profs[i+1]) - int(profs[i])
     delts.append(diff)
     ind_delts[i] = int(diff)
 
+# find the average change from month to month
 avg_ch = sum(delts)/len(delts)
 
+# sort the dictionary by value (profits) in ascending order
 full_sorted = sorted(full_data_no_headers, key=lambda x: x[1])
 
-inc_mo, inc_amt = full_sorted[-1][0], full_sorted[-1][1]
-dec_mo, dec_amt = full_sorted[0][0], full_sorted[0][1]
+# get the month with the greatest profit (increase), get the value of greatest profit (increase)
+inc_mo = full_sorted[-1][0]
+inc_amt = full_sorted[-1][1]
 
-outfile = './analysis/Financial Analysis.txt'
+# get the month with the greatest decrease, get the value of the greatest lost (decrease)
+dec_mo  = full_sorted[0][0], 
+dec_amt = full_sorted[0][1]
+
+# outfile process
+outfile = os.path.join(".", "analysis", "Financial Analysis.txt")
 
 out = open(outfile,'w')
+
 out.write("Financial Analysis")
 out.write("\n----------------------------")
 out.write(f"\nTotal months: {no_months}")
@@ -48,4 +71,5 @@ out.write(f"\nTotal: ${net_total}")
 out.write(f"\nAverage Change: ${round(avg_ch, 2)}")
 out.write(f"\nGreatest Increase in Profits: {inc_mo} (${inc_amt})")
 out.write(f"\nGreatest Decrease in Profits: {dec_mo} (${dec_amt})")
+
 out.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,47 +1,69 @@
 import os
 import csv
 
+# generate file path for input file
 file_path = os.path.join(".","Resources","election_data.csv")
 
 full_data = list()
 
+# open the input file, and for each row of text in csvreader,
+# append the line of text to list full_data
 with open(file_path, encoding='utf-8') as csvf:
     csvreader =  csv.reader(csvf, delimiter=",")
     for i in csvreader:
         full_data.append(i)
 
+# separate the header (header) from the rest of the data (full_data)
 header = full_data[0]
 full_data = full_data[1:]
 
+# total_votes is the total number of votes, equal to the length of the data full_data
 total_votes = len(full_data)
 
 candidate_votes = dict()
 
+# for each row (i) in full_data, i[2] is the candidate's name
 for i in full_data:
+    # if the candidate is not yet in the dictionary as a key, 
+    # add that candidate with initial value (vote count) 1
     if i[2] not in candidate_votes.keys():
         candidate_votes[i[2]] = 1
+    # if the candidate is already in the keys, then 
+    # increment their count by one
     else:
         candidate_votes[i[2]] += 1
 
-candidate_votes['Charles Casper Stockham']
-
 candidate_percentages = dict()
 
+# create a dictionary called candidate_percentages that takes
+# candidate_votes, and divides by the total number of votes
+# and multiplies by 100 to get the percentage as a dictionary
+# with the same keys
 for i in candidate_votes.items():
     candidate_percentages[i[0]] = (candidate_votes[i[0]] / total_votes) *100
 
+# The winner is going to be the candidate with the highest (max)
+# percentage of votes
 winner = max(candidate_percentages, key=candidate_percentages.get)
 
+# generate output file path
 outfile_path = os.path.join(".","analysis","pypoll_analysis.txt")
 
+# output file procedure
 outfile = open(outfile_path, 'w')
+
 outfile.write("Election Results\n")
 outfile.write("-------------------------\n")
 outfile.write(f"Total Votes: {total_votes}\n")
 outfile.write(f"-------------------------\n")
+# in this for loop, the keys for candidate_votes and candidate_percentages
+# are the same set of keys. So, getting the key for one should map to 
+# the keys for the other. This way, we get both the candidate's votes
+# and percentages in one line
 for i in candidate_votes.items():
     outfile.write(f'{i[0]}: {round(candidate_percentages[i[0]],3)}% ({i[1]})\n')
 outfile.write(f"-------------------------\n")
 outfile.write(f"Winner: {winner}\n")
 outfile.write("-------------------------\n")
+
 outfile.close()
